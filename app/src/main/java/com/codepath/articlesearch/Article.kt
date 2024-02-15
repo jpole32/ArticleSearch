bfc27fe7dee7e6ddcdc80d3bef240f274ae08eb6
jpole329 <jpoleary329@gmail.com>
2024-02-15 11:55:27 -0500

    Step 0 finished

--- app/src/main/java/com/codepath/articlesearch/Article.kt
+++ app/src/main/java/com/codepath/articlesearch/Article.kt
@@ -1,1 +1,16 @@
 package com.codepath.articlesearch
+import android.support.annotation.Keep
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Keep
+@Serializable
+data class SearchNewsResponse(
+    @SerialName("response")
+    val response: BaseResponse?
+)
+
+data class Article(){
+    @SerialName("abstract")
+    val newsAb:
+}
