8b9a2be029e39e3ed932f4e18213114caf3def76
jpole329 <jpoleary329@gmail.com>
2024-02-22 23:06:14 -0500

    Started Step 7

--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -3,12 +3,15 @@ package com.codepath.articlesearch
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.articlesearch.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
@@ -31,8 +34,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-
-
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -43,6 +44,22 @@ class MainActivity : AppCompatActivity() {
         // TODO: Set up ArticleAdapter with articles
         val articleAdapter = ArticleAdapter(this, articles)
         articlesRecyclerView.adapter = articleAdapter
+        lifecycleScope.launch {
+            (application as ArticleApplication).db.articleDao().getAll().collect { databaseList ->
+                databaseList.map { entity ->
+                    DisplayArticle(
+                        entity.headline,
+                        entity.articleAbstract,
+                        entity.byline,
+                        entity.mediaImageUrl
+                    )
+                }.also { mappedList ->
+                    articles.clear()
+                    articles.addAll(mappedList)
+                    articleAdapter.notifyDataSetChanged()
+                }
+            }
+        }
         articlesRecyclerView.layoutManager = LinearLayoutManager(this).also {
             val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
             articlesRecyclerView.addItemDecoration(dividerItemDecoration)
@@ -62,9 +79,6 @@ class MainActivity : AppCompatActivity() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
                 Log.i(TAG, "Successfully fetched articles: $json")
                 try {
-                    // TODO: Create the parsedJSON
-
-                    // TODO: Do something with the returned json (contains article information)
 
                     val parsedJson = createJson().decodeFromString(
                         SearchNewsResponse.serializer(),
@@ -73,8 +87,17 @@ class MainActivity : AppCompatActivity() {
 
                     // TODO: Save the articles and reload the screen
                     parsedJson.response?.docs?.let { list ->
-                        articles.addAll(list)
-                        articleAdapter.notifyDataSetChanged()
+                        lifecycleScope.launch(IO) {
+                        (application as ArticleApplication).db.articleDao().deleteAll()
+                        (application as ArticleApplication).db.articleDao().insertAll(list.map {
+                            ArticleEntity(
+                                headline = it.headline?.main,
+                                articleAbstract = it.abstract,
+                                byline = it.byline?.original,
+                                mediaImageUrl = it.mediaImageUrl
+                            )
+                        })
+                    }
                     }
                 } catch (e: JSONException) {
                     Log.e(TAG, "Exception: $e")
@@ -84,4 +107,8 @@ class MainActivity : AppCompatActivity() {
         })
 
     }
+}
+
+private fun <E> MutableList<E>.addAll(elements: List<DisplayArticle>) {
+
 }
